0fb82e4e5965017405844d8039008eb6c8e70c7e
asimsheikh@lidolearning.com <asimsheikh@lidolearning.com>
2021-12-06 23:32:45 +0530

    the updated budget planner web app

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-let suger=document.querySelector('input#suger');
-let rice=document.querySelector('input#rice');
-let butter=document.querySelector('input#butter');
-let totalcost=document.querySelector('h4#total');
-let button=document.querySelector('button#button');
+let budget=prompt('pleae tell us your budget');
+let upbuttons=document.querySelectorAll('input.up');
+upbuttons.forEach(button => {
+  button.addEventListener('click',()=>{
+    
+     let unit=Number(button.previousElementSibling.value);
+     unit+=1;
+     button.previousElementSibling.value=unit;
+    })
+});
 
-
-button.addEventListener('click',()=>{
-  let sugercost=suger.value*40;
-  let ricecost=rice.value*60;
-  let buttercost=butter.value*500;
-  let total=sugercost+ricecost+buttercost;
-
-  totalcost.innerText=total;
+let downbuttons=document.querySelectorAll('input.down');
+downbuttons.forEach(button=>{
+  button.addEventListener('click',()=>{
+    if(button.nextElementSibling.value>0){
+    let unit=Number(button.nextElementSibling.value);
+    unit-=1;
+    button.nextElementSibling.value=unit;
+    }
+  })
 })
- 
+
+
+let cart=document.querySelector('button.cart');
+cart.addEventListener('click',()=>{
+  let quat=document.querySelectorAll('input.num');
+let all=[];
+quat.forEach(n=>{
+  let price=Number(n.getAttribute('price'));
+  let unit=Number(n.value);
+  all.push(Number(price*unit));
+})
+let sumup=all.reduce((total,n)=>{
+  return total+n;
+})
+if(sumup<budget){
+document.querySelector('div.result').innerHTML=`<h1 class="display-4 text-center text-success">
+good job! total ${sumup} is in your budget 
+</h1>`;
+}
+else{
+  document.querySelector('div.result').innerHTML=`<h1 class="display-4 text-center text-danger">
+  the total ${sumup} is going out of your budget but its a good deal!
+  </h1>`;
+
+}
+console.log(sumup);
+})
+
+
+let price=document.querySelector('input.num').getAttribute("price");
+console.log(price+'ddd');
