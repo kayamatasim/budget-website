016113c6413dcd315f715919964d44bffbe2a94e
asimsheikh@lidolearning.com <asimsheikh@lidolearning.com>
2021-12-08 20:48:38 +0530

    final commit ,,the website is ready.

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+ 
+
+fetch("products.json").then((products)=>{
+  return products.json()
+}).then((product)=>{
+  product.forEach(p=>{
+   let row=document.querySelector('div.sec');
+   row.innerHTML+=` <div class="col-sm-3 p-2" style="z-index:2;">
+   <div class="card" style:"z-index:1">
+       <div class="imgbox" style="z-index:1;">   
+                <img src="${p.image}" alt="" class="card-img-top img-fluid">
+       </div>
+       <div class="card-body">
+           <h4 class="card-title my-0">${p.name}</h4>
+
+           <h6>price: ${p.price}rs/kg</h6>
+       </div>
+       <input type="button" class="btn btn-danger btn-sm m-1 mx-2 down" value="-">
+       <input type="button" class="num form btn mx-2" value="0" min="0" price="${p.price}">
+       <input type="button" placeholder="button" value="+" class="btn btn-sm btn-success m-1 mx-2 up">
+   </div>
+</div>`
+
+  incerase();
+  decrease()
+  })
+})
+
+
+
+
 //let budget=prompt('pleae tell us your budget');
 let submit=document.querySelector('button.set');
 let budgetElem=document.querySelector('input.bud');
@@ -7,6 +39,13 @@ let sumup=0;
 submit.addEventListener('click',()=>{
   budget=Number(budgetElem.value);
 })
+
+
+
+//function to increase the product count
+
+function incerase(){
+
 let upbuttons=document.querySelectorAll('input.up');
 upbuttons.forEach(button => {
   button.addEventListener('click',()=>{
@@ -15,60 +54,42 @@ upbuttons.forEach(button => {
      unit+=1;
      button.previousElementSibling.value=unit;
      let total=calc();
-     if(total>budget){
-       document.body.style.backgroundColor='red';
-       alert('you are going out of your set budget');
-     }
+     displayresult()
  })
 });
+}
+
+//function to decrease the product count..
+function decrease(){
 
 let downbuttons=document.querySelectorAll('input.down');
 downbuttons.forEach(button=>{
   button.addEventListener('click',()=>{
+    console.log('clicked')
     if(button.nextElementSibling.value>0){
     let unit=Number(button.nextElementSibling.value);
     unit-=1;
     button.nextElementSibling.value=unit;
 
     let total=calc();
-    if(total<=budget){
-      document.body.style.backgroundColor='white';
-      alert('great you are in budget now');
     }
-    }
+  displayresult()
+
   })
 })
+
+}
+
 
 
 let cart=document.querySelector('a.cart');
 cart.addEventListener('click',()=>{
-  let quat=document.querySelectorAll('input.num');
-let all=[];
-quat.forEach(n=>{
-  let price=Number(n.getAttribute('price'));
-  let unit=Number(n.value);
-  all.push(Number(price*unit));
-})
- sumup=all.reduce((total,n)=>{
-  return total+n;
-})
-if(sumup<budget){
-document.querySelector('div.result').innerHTML=`<h1 class="display-4 text-center text-success">
-good job! total ${sumup} is in your budget.you can do still buy item of worth rs ${budget-sumup}
-</h1>`;
-}
-else{
-  document.querySelector('div.result').innerHTML=`<h1 class="display-4 text-center text-danger">
-  the total ${sumup} is going ${sumup-budget} rs  out of your budget but its a good deal!
-  </h1>`;
+ displayresult();
 
-}
-console.log(sumup);
 })
 
 
-let price=document.querySelector('input.num').getAttribute("price");
-console.log(price+'ddd');
+
 
 function calc(){
   let quat=document.querySelectorAll('input.num');
@@ -84,3 +105,36 @@ function calc(){
 
   return sumup;
 }
+
+function displayresult(){
+  let quat=document.querySelectorAll('input.num');
+  let all=[];
+  quat.forEach(n=>{
+    let price=Number(n.getAttribute('price'));
+    let unit=Number(n.value);
+    all.push(Number(price*unit));
+  })
+   sumup=all.reduce((total,n)=>{
+    return total+n;
+  })
+  if(sumup<budget){
+    document.querySelector('div.result').innerHTML=`<h1 class="display-sm-4 text-center text-dark">
+  good job! total ${sumup} is in your budget.you can do still buy item of worth rs ${budget-sumup}
+  </h1>`;
+  }
+  else if (sumup==budget) {
+    document.querySelector('div.result').innerHTML=`<h1 class="display-sm-4 text-center text-success">
+    perfect! the total ${sumup} is exactly equal to your budget
+    </h1>`;
+  } 
+  else{
+  document.querySelector('div.result').innerHTML=`<h1 class="display-sm-4 text-center text-danger">
+  the total ${sumup} is going ${sumup-budget} rs  out of your budget but its a good deal!
+  </h1>`;
+  
+  }
+  console.log(sumup);
+  }
+  
+
+
